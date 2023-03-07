43d1cc480e274c7569e52350f12dfa54ed786e82
mirza zanuar <zanuarmirza@gmail.com>
2023-03-07 12:49:39 +0700

    add vim gitgutter, adding git changes indicator

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -204,6 +204,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/catalyst/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-gitgutter"] = {
+    loaded = true,
+    path = "/Users/catalyst/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    url = "https://github.com/airblade/vim-gitgutter"
   }
 }
 
