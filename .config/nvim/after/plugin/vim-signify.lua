ea1d08187cb242a2344aee2e3af58a48b62bebb2
mirza zanuar <zanuarmirza@gmail.com>
2023-03-10 10:54:51 +0700

    adding vim signify

--- .config/nvim/after/plugin/vim-signify.lua
+++ .config/nvim/after/plugin/vim-signify.lua
@@ -1,5 +1,7 @@
 vim.keymap.set("n", "<leader>gd", ":SignifyDiff<CR>", {})
-vim.keymap.set("n", "]h", "<Plug>(signify-next-hunk)", {})
-vim.keymap.set("n", "[h", "<Plug>(signify-prev-hunk)", {})
+vim.keymap.set("n", "<leader>gh", ":SignifyHunkDiff<CR>", {})
+vim.keymap.set("n", "<leader>gu", ":SignifyHunkUndo<CR>", {})
+vim.keymap.set("n", "]h", "<plug>(signify-next-hunk)", {})
+vim.keymap.set("n", "[h", "<plug>(signify-prev-hunk)", {})
 
 
