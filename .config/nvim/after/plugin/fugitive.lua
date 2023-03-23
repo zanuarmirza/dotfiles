3014f91f765104bcb63f9dd466c23912c66c7d06
mirza zanuar <zanuarmirza@gmail.com>
2023-03-23 10:27:22 +0700

    git diff work

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,8 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+vim.keymap.set("n", "<leader>gl", ":0Gclog -n1000<CR>")
+vim.keymap.set("n", "<leader>gL", ":0Gclog<CR>")
+-- used nicely with gclog, show the vsplitdiff on current diff file
+vim.keymap.set("n", "<leader>df", ":Gvdiffsplit<CR>")
+-- used nicely with git blame, by placing the cursor to the commit hash in git blame
+vim.keymap.set("n", "<leader>dF", ":Gvdiffsplit <C-r><C-w><CR>")
