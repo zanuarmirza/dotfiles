0a184e59f2cff6387ae52ddefb3933a7f19f587d
mirza zanuar <zanuarmirza@gmail.com>
2023-04-26 10:16:17 +0700

    update lsp nvim keybind

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -56,14 +56,16 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vf", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     -- The following command requires plug-ins "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", and optionally "kyazdani42/nvim-web-devicons" for icon support
     vim.api.nvim_set_keymap('n', '<leader>vd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
     -- If you don't want to use the telescope plug-in but still want to see all the errors/warnings, comment out the telescope line and uncomment this:
-    vim.api.nvim_set_keymap('n', '<leader>vD', '<cmd>lua vim.diagnostic.setqflist()<CR>',
+    vim.api.nvim_set_keymap('n', '<leader>VD', '<cmd>lua vim.diagnostic.setqflist()<CR>',
+        { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>VE',
+        '<cmd>lua vim.diagnostic.setqflist({severity=vim.diagnostic.severity.ERROR})<CR>',
         { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>li', '<cmd>EslintFixAll<CR>', { noremap = true, silent = true })
 end)
