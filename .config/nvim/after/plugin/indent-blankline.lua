9cbfe836b1b9894a5f6bd496c1268cd133154cca
mirza zanuar <zanuarmirza@gmail.com>
2023-05-09 15:59:33 +0700

    update indent config

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,14 +1,12 @@
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#50fa7b gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#ffffff gui=nocombine]]
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1"
-    },
+    char_highlight_list = " ",
     space_char_highlight_list = " ",
-    show_trailing_blankline_indent = false,
-    show_current_context = true,
-    show_current_context_start = true,
+    -- show_trailing_blankline_indent = false,
+    -- show_current_context = true,
+    -- show_current_context_start = true,
 }
