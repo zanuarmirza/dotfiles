07dfaa645cc70a8eef11bad7b7d1a738300f0273
Mirza <zanuarmirza@gmail.com>
2023-08-20 10:58:48 +0700

    update nvim

--- .config/nvim/after/plugin/vim-signify.lua
+++ .config/nvim/after/plugin/vim-signify.lua
@@ -1,7 +1,4 @@
-vim.keymap.set("n", "<leader>gd", ":SignifyDiff<CR>", {})
 vim.keymap.set("n", "<leader>gh", ":SignifyHunkDiff<CR>", {})
 vim.keymap.set("n", "<leader>gu", ":SignifyHunkUndo<CR>", {})
-vim.keymap.set("n", "]h", "<plug>(signify-next-hunk)", {})
-vim.keymap.set("n", "[h", "<plug>(signify-prev-hunk)", {})
 
 
