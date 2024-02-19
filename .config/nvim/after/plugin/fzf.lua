0c8defc52e8695b0ec82029755e88bb0fac533a6
Mirza <zanuarmirza@gmail.com>
2024-02-19 19:05:04 +0700

    fzf-lua hidden files

--- .config/nvim/after/plugin/fzf.lua
+++ .config/nvim/after/plugin/fzf.lua
@@ -1,43 +1,48 @@
 require("fzf-lua").setup({ "telescope" })
 local actions = require("fzf-lua.actions")
-local m = require("fzf-lua",{
-    files= {
-        { fzf_opts = { ['--keep-right'] = '' }}
+local m = require("fzf-lua", {
+    files   = {
+        { fzf_opts = { ['--keep-right'] = '' } }
     },
-    actions    = {
+    actions = {
         files = {
-            ["ctrl-s"]  = actions.file_split,
+            ["ctrl-s"] = actions.file_split,
         },
         buffers = {
-            ["ctrl-s"]  = actions.buf_split,
+            ["ctrl-s"] = actions.buf_split,
         }
     }
 })
 
-vim.keymap.set('n', '<leader>ff', m.files, { silent = true }) -- find files (fuzzy)
+vim.keymap.set('n', '<leader>ff', m.files, { silent = true })   -- find files (fuzzy)
 vim.keymap.set('n', '<leader>fb', m.buffers, { silent = true }) -- show buffer
-vim.keymap.set('n', '<C-p>', m.git_files, {silent = true}) -- search files at git
-vim.keymap.set('n', '<C-P>', m.commands, {silent = true}) -- search files at git
-vim.keymap.set('n', '<leader>fr', m.resume, {}) -- show latest telescope result
+vim.keymap.set('n', '<C-p>', m.git_files, { silent = true })    -- search files at git
+vim.keymap.set('n', '<C-P>', m.commands, { silent = true })     -- search files at git
+vim.keymap.set('n', '<leader>fr', m.resume, {})                 -- show latest telescope result
 -- vim.keymap.set('n', '<leader>fs', m.lsp_document_symbols, {}) -- show documents symbols
-vim.keymap.set('n', '<leader>gt', m.git_status, {}) -- show git status list
-vim.keymap.set('n', '<leader>f/', m.lgrep_curbuf, {}) -- show git status list
+vim.keymap.set('n', '<leader>gt', m.git_status, {})             -- show git status list
+vim.keymap.set('n', '<leader>f/', m.lgrep_curbuf, {})           -- show git status list
 vim.keymap.set('n', '<leader>o', function()
     m.oldfiles({
-        prompt_title = 'Project History',
-        cwd_only = true,
-        stat_file         = true,         -- verify files exist on disk
-        include_current_session = true,  -- include bufs from current session
+        prompt_title            = 'Project History',
+        cwd_only                = true,
+        stat_file               = true, -- verify files exist on disk
+        include_current_session = true, -- include bufs from current session
     })
-end, {}) -- show oldfiles
+end, {})                                -- show oldfiles
 
 vim.keymap.set('n', '<leader>fg', function()
     m.grep({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>fG', function()
+    m.grep({ search = vim.fn.input("Grep > "), rg_opts = '--line-number --column --color=always --hidden' });
+end)
+
 vim.keymap.set('n', '<leader>flg', function()
     m.live_grep();
 end)
 
 vim.keymap.set('n', '<leader>flG', function()
-    m.live_grep({ rg_opts = '--multiline' });
+    m.live_grep({ rg_opts = '--multiline --line-number --column --color=always --hidden' });
 end)
