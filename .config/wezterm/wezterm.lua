7c8c9541dafd8e9e607db6b01a3f0af4436df36c
Mirza <zanuarmirza@gmail.com>
2024-08-01 09:51:15 +0700

    update i3 and add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+local config ={}
+config.color_scheme = 'Dracula+'
+config.font = wezterm.font 'Agave Nerd Font'
+config.font_size = 13
+config.window_background_opacity = 0.8
+return config
