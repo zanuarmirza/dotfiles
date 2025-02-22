9be52398a6bd62041302cc2a8ec2808035d4449f
Mirza <zanuarmirza@gmail.com>
2025-02-22 16:13:51 +0700

    update outline

--- .config/nvim/lua/plugins/symbols-outline.lua
+++ .config/nvim/lua/plugins/symbols-outline.lua
@@ -1,6 +1,12 @@
-return {
-	"simrat39/symbols-outline.nvim",
-	config = function()
-		vim.keymap.set("n", "<leader>fs", ":SymbolsOutline<CR>")
-	end,
+return{
+  "hedyhli/outline.nvim",
+  config = function()
+    -- Example mapping to toggle outline
+    vim.keymap.set("n", "<leader>fs", "<cmd>Outline<CR>",
+      { desc = "Toggle Outline" })
+
+    require("outline").setup {
+      -- Your setup opts here (leave empty to use defaults)
+    }
+  end,
 }
